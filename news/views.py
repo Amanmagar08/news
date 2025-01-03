453e1994ebf82cd73ca3622854935ce82ce015b0
Aman thapa magar <magaraman984@gmail.com>
2025-01-03 15:32:31 +0545

    day 2 Navbar

--- news/views.py
+++ news/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "index.html")
+    dict = {
+          'name' : 'item',
+          'price': '$999'
+        }
+
+    return render(request, "index.html", dict)
+
+
+def about(request):
+    return render(request,'about.html')
